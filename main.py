89d268a3804dddec960c771020c6f510a8669c34
NoePfister <noe.pfister@gmx.ch>
2024-05-12 18:03:51 +0200

    finished base game

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ class Game:
     def __init__(self, dimensions):
         self.dimensions = dimensions
         self.setup()
-        self.player1 = Player(True,self.canvas)
-        self.player2 = Player(False, self.canvas)
-        self.ball = Ball(self.canvas)
+        self.player1 = Player(True,self.canvas, self)
+        self.player2 = Player(False, self.canvas, self)
+        self.ball = Ball(self.canvas, self)
         self.loop()
 
 
@@ -46,60 +46,28 @@ class Game:
             self.player2.draw()
             self.ball.draw()
             pg.display.update()
+
+    def end(self):
+        print("Game Finished")
+        self.quit()
+
     def quit(self):
         pg.quit()
 
 
-class Ball:
-    def __init__(self,screen):
-        self.screen = screen
-        self.pos = [500,250]
-        self.cross_pos = [500,250]
-        self.rad = 10
-        self.start_pos = [500,250]
-        self.velocity = [1,0]
-
-        self.setup()
-
-    def setup(self):
-        self.velocity=[1,random.uniform(0,1)]
-
-    def update(self):
-        self.pos[1]+=self.velocity[1]/10
-        self.pos[0]+=self.velocity[0]/10
-        self.check_collide()
-
-    def draw(self):
-        pg.draw.circle(self.screen,(255,255,255),self.pos,self.rad)
-
-    def check_collide(self):
-    # When Ball collides with Player, multiply the x velocity by -1
-    # When Ball collides with Wall, multiply the y velocity by -1
-    # When Ball collides with Sides, end the game
-        if(self.pos[0] < self.rad) or (self.pos[0] > 1000-self.rad):
-            self.end()
-            print(0)
-        if self.pos[0] < 11 or self.pos[0] > 989:
-            self.velocity[0] = self.velocity[0]*(-1)
-        if (self.pos[1] < 0) or (self.pos[1] > 500):
-            self.velocity[1] = self.velocity[1]*(-1)
-            
-
-    def end(self):
-        pg.quit()
- 
 
         
 
 
 
 class Player:
-    def __init__(self,is_left: bool, screen):
+    def __init__(self,is_left: bool, screen, game :Game):
         self.is_left=is_left
         self.screen = screen
         self.pos = [0,0]
         self.width = 10
         self.height = 100
+        self.game = game
         self.setup()
 
     def setup(self):
@@ -122,6 +90,64 @@ class Player:
 def start():
     dimensions = (1000,500)
     game = Game(dimensions)
+
+
+class Ball:
+    def __init__(self,screen, game: Game):
+        self.screen = screen
+        self.pos = [500,250]
+        self.rad = 10
+        self.velocity = [1,0]
+        self.game = game
+
+        self.setup()
+
+    def setup(self):
+        self.velocity=[1,random.uniform(0,1)]
+
+    def update(self):
+        self.pos[1]+=self.velocity[1]/10
+        self.pos[0]+=self.velocity[0]/10
+        self.check_collide()
+
+    def draw(self):
+        pg.draw.circle(self.screen,(255,255,255),self.pos,self.rad)
+
+    def check_collide(self):
+        # Todo: Check if it collides with the player
+    # When Ball collides with Player, multiply the x velocity by -1
+    # When Ball collides with Wall, multiply the y velocity by -1
+    # When Ball collides with Sides, end the game
+
+        # Check if Ball collides with player.
+        if self.check_player_collison(self.game.player1) or self.check_player_collison(self.game.player2):
+            self.velocity[0] *= -1
+            print("Player Collision")
+            return
+        # Check if Ball is hitting the Wall
+        if (self.pos[1] < 0+self.rad) or (self.pos[1] > 500-self.rad):
+            self.velocity[1] *= -1 
+            print("Wall Collision")
+        # Check if Ball is out of Bounds.
+        if(self.pos[0] < self.rad) or (self.pos[0] > 1000-self.rad):
+            print("Out of bounds")
+            self.end()
+
+    def check_player_collison(self,player: Player):
+        #Check if ball is traveling away from Player
+        if player.is_left and self.velocity[0] > 0: return False
+        if not player.is_left and self.velocity[0] < 0: return False
+        # Check if Ball is in Range of Player
+        if self.pos[0] < 990-self.rad and self.pos[0] > 10+self.rad: return False
+        # Check if y value is under the Player
+        if self.pos[1] > player.pos[1]+player.height: return False
+        # Check if y value is above the Player
+        if self.pos[1] < player.pos[1]: return False
+        return True
+            
+
+    def end(self):
+        self.game.end() 
 
 if __name__ == "__main__":
     start()
