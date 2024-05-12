d0fb420b3e6b15d0faaaa42dd2f27bc5d90b8727
NoePfister <noe.pfister@gmx.ch>
2024-05-12 15:38:53 +0200

    added Ball

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 
 class Game:
     def __init__(self, dimensions):
@@ -6,6 +7,7 @@ class Game:
         self.setup()
         self.player1 = Player(True,self.canvas)
         self.player2 = Player(False, self.canvas)
+        self.ball = Ball(self.canvas)
         self.loop()
 
 
@@ -36,14 +38,58 @@ class Game:
             if keys[pg.K_w]: self.player1.move(-1)
 
 
+            self.ball.update()
 
             self.canvas.fill((0,0,0))
-            pg.draw.rect(self.canvas,(200,200,200),(500,0,10,500))
+            pg.draw.rect(self.canvas,(100,100,100),(500,0,10,500))
             self.player1.draw()
             self.player2.draw()
+            self.ball.draw()
             pg.display.update()
     def quit(self):
         pg.quit()
+
+
+class Ball:
+    def __init__(self,screen):
+        self.screen = screen
+        self.pos = [500,250]
+        self.cross_pos = [500,250]
+        self.rad = 10
+        self.start_pos = [500,250]
+        self.velocity = [1,0]
+
+        self.setup()
+
+    def setup(self):
+        self.velocity=[1,random.uniform(0,1)]
+
+    def update(self):
+        self.pos[1]+=self.velocity[1]/10
+        self.pos[0]+=self.velocity[0]/10
+        self.check_collide()
+
+    def draw(self):
+        pg.draw.circle(self.screen,(255,255,255),self.pos,self.rad)
+
+    def check_collide(self):
+    # When Ball collides with Player, multiply the x velocity by -1
+    # When Ball collides with Wall, multiply the y velocity by -1
+    # When Ball collides with Sides, end the game
+        if(self.pos[0] < self.rad) or (self.pos[0] > 1000-self.rad):
+            self.end()
+            print(0)
+        if self.pos[0] < 11 or self.pos[0] > 989:
+            self.velocity[0] = self.velocity[0]*(-1)
+        if (self.pos[1] < 0) or (self.pos[1] > 500):
+            self.velocity[1] = self.velocity[1]*(-1)
+            
+
+    def end(self):
+        pg.quit()
+ 
+
+        
 
 
 
